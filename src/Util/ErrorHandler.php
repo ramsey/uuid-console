de3abb276994e8f0422e25c6c552718ccc336c23
Ben Ramsey <ben@benramsey.com>
2015-09-28 12:41:23 -0500

    Update common file header information

--- src/Util/ErrorHandler.php
+++ src/Util/ErrorHandler.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * This file is part of the Ramsey\Uuid library
+ * This file is part of the ramsey/uuid-console application
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @copyright Copyright (c) 2012-2014 Ben Ramsey <http://benramsey.com>
+ * @copyright Copyright (c) Ben Ramsey <ben@benramsey.com>
  * @license http://opensource.org/licenses/MIT MIT
+ * @link https://packagist.org/packages/ramsey/uuid-console Packagist
+ * @link https://github.com/ramsey/uuid-console GitHub
  */
 
 namespace Ramsey\Uuid\Console\Util;
--- src/Util/UuidContentFormatterInterface.php
+++ src/Util/UuidContentFormatterInterface.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of the ramsey/uuid-console application
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Ben Ramsey <ben@benramsey.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @link https://packagist.org/packages/ramsey/uuid-console Packagist
+ * @link https://github.com/ramsey/uuid-console GitHub
+ */
 
 namespace Ramsey\Uuid\Console\Util;
 
